4e6dc6adb28b5db19ca7304dd160ff96ef61f575
幼儿园laoda <1378194676@qq.com>
2023-07-28 23:43:27 +0800

    C语言集训营

--- test_7_28/test_7_28/test.c
+++ test_7_28/test_7_28/test.c
@@ -187,12 +187,280 @@ KiKi有一个矩阵，他想知道转置后的矩阵（将矩阵的行列互换得到的新矩阵称为转置矩阵）
  */
 
 
+//#include "stdio.h"
+//int main() {
+//    for (int year = 1000; year <= 2000; year++) {
+//        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+//            printf("%d ", year);
+//        }
+//    }
+//    return 0;
+//}
+
+
+
+
+
+
+/*
+描述
+KiKi学习了循环，BoBo老师给他出了一系列打印图案的练习，该任务是打印用“*”组成的“空心”正方形图案。
+输入描述：
+多组输入，一个整数（3~20），表示输出的行数，也表示组成正方形边的“*”的数量。
+输出描述：
+针对每行输入，输出用“*”组成的“空心”正方形，每个“*”后面有一个空格。
+示例1
+输入：
+4
+输出：
+* * * *
+*     *
+*     *
+* * * *
+
+示例2
+输入：
+5输出：
+* * * * *
+*       *
+*       *
+*       *
+* * * * *
+*/
+
+
+
+//#include <stdio.h>
+//
+//void print(int num) {
+//    int j, i;
+//    for (j = 0; j < num; j++) {
+//        for (i = 0; i < num; i++) {
+//            if (j == 0 || i == 0 || j == num - 1 || i == num - 1) {
+//                printf("* ");
+//            }
+//            else {
+//                printf("  ");
+//            }
+//        }
+//        printf("\n");
+//    }
+//}
+//
+//int main() {
+//    int num;
+//    while (scanf("%d", &num) != EOF) {
+//        print(num);
+//    }
+//    return 0;
+//}
+
+
+
+
+
+/*
+描述
+KiKi学习了循环，BoBo老师给他出了一系列打印图案的练习，该任务是打印用“*”组成的X形图案。
+输入描述：
+多组输入，一个整数（2~20），表示输出的行数，也表示组成“X”的反斜线和正斜线的长度。
+输出描述：
+针对每行输入，输出用“*”组成的X形图案。
+
+示例1
+输入：
+5输出：
+*   *
+ * *
+  *
+ * *
+*   *
+示例2
+输入：
+6输出：
+*    *
+ *  *
+  **
+  **
+ *  *
+*    *
+*/
+
+
+
+//#include <stdio.h>
+//
+//void print(int num) {
+//    int j, i;
+//    for (j = 0; j < num; j++) {
+//        for (i = 0; i < num; i++) {
+//            if (j == i || (j + i == num - 1)) {
+//                printf("*");
+//            }
+//            else {
+//                printf(" ");
+//            }
+//        }
+//        printf("\n");
+//    }
+//
+//}
+//
+//int main() {
+//    int num;
+//    while (scanf("%d", &num) != EOF) {
+//        print(num);
+//    }
+//    return 0;
+//}
+
+
+
+
+
+/*
+描述
+输入10个整数，要求按输入时的逆序把这10个数打印出来。逆序输出，就是按照输入相反的顺序打印这10个数。
+输入描述：
+一行，输入10个整数（范围-231~231-1），用空格分隔。
+输出描述：
+一行，逆序输出输入的10个整数，用空格分隔。
+示例1
+输入：
+1 2 3 4 5 6 7 8 9 10
+复制
+输出：
+10 9 8 7 6 5 4 3 2 1
+*/
+
+//#include <stdio.h>
+//
+//int main() {
+//    int arr[10];
+//    for (int i = 0; i < sizeof(arr) / sizeof(arr[0]); i++) {
+//        scanf("%d", &arr[i]);
+//    }
+//    for (int i = sizeof(arr) / sizeof(arr[0]) - 1; i >= 0; i--) {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
+
+
+
+
+/*
+描述
+输入两个升序排列的序列，将两个序列合并为一个有序序列并输出。
+数据范围： 1 \le n, m \le 1000 \1≤n,m≤1000  ， 序列中的值满足 0 \le val \le 30000 \0≤val≤30000
+输入描述：
+输入包含三行，
+第一行包含两个正整数n, m，用空格分隔。n表示第二行第一个升序序列中数字的个数，m表示第三行第二个升序序列中数字的个数。
+第二行包含n个整数，用空格分隔。
+第三行包含m个整数，用空格分隔。
+输出描述：
+输出为一行，输出长度为n+m的升序序列，即长度为n的升序序列和长度为m的升序序列中的元素重新进行升序序列排列合并。
+示例1
+输入：
+5 6
+1 3 7 9 22
+2 8 10 17 33 44
+输出：
+1 2 3 7 8 9 10 17 22 33 44
+ */
+
+//#include <stdio.h>
+//
+//int main() {
+//    int num1 = 0, num2 = 0;
+//    int j = 0, i = 0;
+//    int arr[2000];
+//    int tmp;
+//    scanf("%d %d", &num1, &num2);
+//    for (i = 0; i < num1; i++) {
+//        scanf("%d", &arr[i]);
+//    }
+//    for (i = num1; i < num1 + num2; i++) {
+//        scanf("%d", &arr[i]);
+//    }
+//    for (i = 0; i < num1 + num2; i++) {
+//        for (j = 0; j < num1 + num2 - i - 1; j++) {
+//            if (arr[j] > arr[j + 1]) {
+//                tmp = arr[j];
+//                arr[j] = arr[j + 1];
+//                arr[j + 1] = tmp;
+//            }
+//        }
+//    }
+//    for (i = 0; i < num1 + num2; i++) {
+//        printf("%d ", arr[i]);
+//    }
+//    return 0;
+//}
+
+
+
+
+
+/*
+ 作业标题(680)
+函数判新素数
+四作业内容
+实现一个函数is_prime,判断一个数是不是素数。
+利用上面实现的is primel函数，打印100到200之间的素数。
+ */
+
+//#include "stdio.h"
+//
+//int is_prime(int num) {
+//    for (int i = 2; i < num; i++) {
+//        if (num % i == 0) {
+//            return 0;
+//        }
+//    }
+//    return 1;
+//}
+//
+//int main() {
+//    for (int i = 100; i <= 200; i++) {
+//        int flag = is_prime(i);
+//        if (flag) {
+//            printf("%d ", i);
+//        }
+//    }
+//    return 0;
+//}
+
+
+
+
+/*
+ * 作业标题(679)
+函数判断闰年
+作业内容
+实现函数判新year是不是润年。
+ */
+
+
 #include "stdio.h"
+
+int is_year(int year) {
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
+        return 1;
+    }
+    return 0;
+}
+
 int main() {
-    for (int year = 1000; year <= 2000; year++) {
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            printf("%d ", year);
-        }
+    int year;
+    printf("请输入年份\n");
+    scanf("%d", &year);
+    int flag = is_year(year);
+    if (flag) {
+        printf("%d是闰年", year);
+    }
+    else {
+        printf("%d不是闰年", year);
     }
     return 0;
 }
